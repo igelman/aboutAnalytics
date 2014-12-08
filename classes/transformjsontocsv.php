2d173f867ca3c04bd529540c5235c8643f3797a9
Alan Igelman <alan@igelman.com>
2014-12-07 21:06:52 -0500

    Add sumKeyValues method.

--- classes/transformjsontocsv.php
+++ classes/transformjsontocsv.php
@@ -3,11 +3,31 @@
 class TransformJsonToCsv {
 
   public function __construct($json) {
-    $this->json = $json;
+    $this->json = utf8_encode($json);
+    if (!json_decode($this->json)) {
+      echo "TransformJsonToCSV could not decode the json. " . json_last_error_msg() . PHP_EOL . $this->json . PHP_EOL . PHP_EOL;
+    }
   }
 
   public function getJson() {
     return $this->json;
+  }
+
+  public function sumKeyValues($key) {
+    $pages = json_decode($this->json);
+    $sums = [];
+    // foreach ($pages as $pageObject) {
+    //   // not sure why I need to do this
+    //   // but if I don't, the next forEach complains
+    //   $sums[$pageObject->_id->url] = 0;
+    // }
+    foreach ($pages as $pageObject) {
+      if (!array_key_exists ( $pageObject->_id->url , $sums )) {
+        $sums[$pageObject->_id->url] = 0;
+      }
+      $sums[$pageObject->_id->url] += $pageObject->pvs;
+    }
+   return $sums;
   }
 }
 
--- tests/test-transform-json-to-csv.php
+++ tests/test-transform-json-to-csv.php
@@ -19,13 +19,24 @@ class TestTransformJsonToCsv extends PHPUnit_Framework_TestCase {
 	public function testGetJsonReturnsJson() {
 		$array = json_decode($this->tjtc->getJson());
 		$junk = json_decode($this->tjtcJunk->getJson());
-		$this->assertNotNull($array);
+		$this->assertNotNull($array, "getJson returned" . print_r($array, TRUE) . PHP_EOL);
 		$this->assertNull($junk);
 	}
 	
-// 	public function testSumKeyValues() {
-// 		$this->assertTrue();
-// 	}
+ 	public function testSumKeyValues() {
+ 		$pages = json_decode($this->response, TRUE);
+// 		echo print_r($pages, TRUE);
+ 		foreach($pages as $page) {
+ 			$result[$page["_id"]["url"]] = $page["pvs"];
+ 			$urls[] = $page["_id"]["url"];
+ 		}
+// 		echo "testSumKeyValues " . print_r($result, TRUE) . PHP_EOL;
+		$key = "pvs";
+		$sums = $this->tjtc->sumKeyValues($key);
+		echo "sumKeyValues returns sums: " . print_r($sums, TRUE) . PHP_EOL;
+ 		$this->assertEquals($result[$urls[0]], $sums[$urls[0]]);
+ 		$this->assertEquals($result[$urls[1]], $sums[$urls[1]]);
+ 	}
 	
 	
 }
